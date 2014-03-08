932d8fc232cbb36d185370b5013eeec46c2977fb
pyemkey <py.emkey@gmail.com>
2014-03-08 16:29:37 +0100

    Fix association

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,5 +1,6 @@
 class Game < ActiveRecord::Base
-  has_many :teams
+  belongs_to :team1, class_name: "Team"
+  belongs_to :team2, class_name: "Team"
   attr_accessor :team1_name, :team2_name
   validates_presence_of :name
 end
