658631825b00939dffe44e9306b25584a5153006
pyemkey <py.emkey@gmail.com>
2014-03-08 16:31:33 +0100

    Create new game

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,6 +1,29 @@
 class GamesController < ApplicationController
-  expose :game
+  expose :game, attributes: :game_attributes
   def create
     
+    team1 = Team.new name: params[:game][:team1_name]
+    team2 = Team.new name: params[:game][:team2_name]
+
+    game.team1 = team1
+
+    game.team2 = team2
+
+    if game.save
+      redirect_to game
+    else
+      render :new
+    end
+    #binding.pry
   end
+
+  def show
+    
+  end
+
+  private
+    def game_attributes
+      params.require(:game).permit(:name)
+    end
 end
+
