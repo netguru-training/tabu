a5e96495ce4a49af7c2551c53e9856be1e6e7cbb
pyemkey <py.emkey@gmail.com>
2014-03-08 14:30:00 +0100

    Add game model

---
+++ app/models/game.rb
@@ -0,0 +1,4 @@
+class Game < ActiveRecord::Base
+  has_many :teams
+  validates_presence_of :name
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140308105017) do
+ActiveRecord::Schema.define(version: 20140308123813) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "cards", force: true do |t|
     t.string   "title"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "games", force: true do |t|
+    t.string   "name"
+    t.integer  "team1_id"
+    t.integer  "team2_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
