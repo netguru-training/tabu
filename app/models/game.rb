c39a70feb71d19e5071f5f762cd59d6faffbc5a0
pyemkey <py.emkey@gmail.com>
2014-03-08 17:18:19 +0100

    Display team names

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,6 +1,12 @@
 class Game < ActiveRecord::Base
   belongs_to :team1, class_name: "Team"
   belongs_to :team2, class_name: "Team"
+
   attr_accessor :team1_name, :team2_name
+  
   validates_presence_of :name
+
+  def complete_name
+    "#{team1.name} vs #{team2.name}"
+  end
 end
