88c90baad7d47d9845ad501059a8890c9bf8e1d7
pyemkey <py.emkey@gmail.com>
2014-03-09 15:07:39 +0100

    Add before_filters fo r voting and sample_cards

--- app/controllers/game_cards_controller.rb
+++ app/controllers/game_cards_controller.rb
@@ -1,21 +1,24 @@
 class GameCardsController < ApplicationController
   expose :game_card
-  
+  before_filter :vote, only: :next_round
+  before_filter :sample_card, only: :next_round
   def next_round
 
     game_card.game = current_game
     game_card.team = select_team
-    game_card.card = sample_card
-    if params[:result] == 1
-      game_card.set_as_correct!
-    else
-      game_card.set_as_wrong!
-    end
+    game_card.card = @sample_card
     game_card.save
-
+    set_last_game_card(game_card)
   end
 
   private
+    def vote
+      if params[:result] == "1"
+        last_game_card.set_as_correct!
+      elsif params[:result] == "-1"
+        last_game_card.set_as_wrong!
+      end
+    end
 
     def select_team
       current_game.game_cards.count % 10 > 5 ? current_game.team1 : current_game.team2
@@ -23,8 +26,7 @@ class GameCardsController < ApplicationController
 
 
     def sample_card
-
-      Card.order("random()").to_a.delete_if { |c| current_game.game_cards.map(&:card_id).include? c.id }.first
-
+      @sample_card = Card.order("random()").to_a.delete_if { |c| current_game.game_cards.map(&:card_id).include? c.id }.first
+      redirect_to game_results_path(current_game) unless @sample_card
     end
 end
