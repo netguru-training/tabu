782f7bc6f8897e2b58a4685aa4b8cd928f680b62
Wojciech Łoza <wojciechloza@gmail.com>
2014-03-09 17:00:07 +0100

    Added rspec test cases for games_controller

--- spec/controllers/games_controller_spec.rb
+++ spec/controllers/games_controller_spec.rb
@@ -1,5 +1,39 @@
 require 'spec_helper'
 
+shared_examples_for 'valid' do
+  it "adds two new teams" do
+    expect {
+      post :create, game: @game_params
+      }.to change(Team.all, :count).by(2)
+  end
+  it "creates new game once" do
+    expect {
+      post :create, game: @game_params
+      }.to change(Game.all, :count).by(1)
+  end
+  it "should redirect to game/:id page" do
+    post :create, game: @game_params
+    response.should redirect_to game_path(controller.game)
+  end
+end
+
+shared_examples_for 'invalid' do
+  it "does not add two new teams" do
+    expect {
+      post :create, game: @game_params
+      }.not_to change(Team.all, :count).by(2)
+  end
+  it "does not create new game once" do
+    expect {
+      post :create, game: @game_params
+      }.not_to change(Game.all, :count).by(1)
+  end
+  it "should render new game form again" do
+    post :create, game: @game_params
+    response.body.should render_template("games/new")
+  end
+end
+
 describe GamesController do
   describe "POST create" do
     before do
@@ -10,22 +44,7 @@ describe GamesController do
           }
     end
     context "good params" do
-      
-      it "adds two new teams" do
-        expect {
-          post :create, game: @game_params
-          }.to change(Team.all, :count).by(2)
-      end
-      it "creates new game once" do
-        expect {
-          post :create, game: @game_params
-          }.to change(Game.all, :count).by(1)
-      end
-      it "should redirect to game/:id page" do
-        post :create, game: @game_params
-        response.should redirect_to game_path(controller.game)
-      end
-      
+      it_behaves_like 'valid', true
     end
     context "empty" do
       context "all fields" do
@@ -35,21 +54,26 @@ describe GamesController do
             :team2_name => "",
             :name => ""
             }
+      end
+      context "game name field empty" do
+        before do
+          @game_params[:name] = ""
         end
-        it "not adds two new teams" do
-          expect {
-            post :create, game: @game_params
-            }.not_to change(Team.all, :count).by(2)
+         it_behaves_like 'invalid', true
+      end
+      context "Team name field empty" do
+        before do
+          @game_params[:team1_name] = ""
+          @game_params[:team2_name] = ""
         end
-        it "not creates new game once" do
-          expect {
-            post :create, game: @game_params
-            }.not_to change(Game.all, :count).by(1)
+        it_behaves_like 'invalid', true
+      end
+    end
+    context "One of team name field empty" do
+        before do
+          @game_params[:team1_name] = ""
         end
-        it "should render new game form again" do
-          post :create, game: @game_params
-          response.body.should render_template("games/new")
-        end
+        it_behaves_like 'invalid', true
       end
     end
   end
