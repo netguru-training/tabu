7972bc21f2d9b08c7e1865bc4c7af7352a3910d3
Wojciech Łoza <wojciechloza@gmail.com>
2014-03-09 16:28:27 +0100

    Repaired has_many :game_cards

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,6 +1,7 @@
 class Game < ActiveRecord::Base
   belongs_to :team1, class_name: "Team"
   belongs_to :team2, class_name: "Team"
+  has_many :game_cards 
 
   attr_accessor :team1_name, :team2_name
   
