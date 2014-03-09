92dd59356519788a166eb0fe9e9b68d39096fa91
pyemkey <py.emkey@gmail.com>
2014-03-09 11:42:45 +0100

    Update results

--- app/models/game_card.rb
+++ app/models/game_card.rb
@@ -2,4 +2,12 @@ class GameCard < ActiveRecord::Base
   belongs_to :card
   belongs_to :game
   belongs_to :team
+
+  def set_as_correct!
+    update_attribute(:result, 1) 
+  end
+
+  def set_as_wrong!
+    update_attribute(:result, -1)
+  end
 end
