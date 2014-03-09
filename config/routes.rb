ab7a5192e63e89d78611ff5c5d1b5ec184ed1e6c
pyemkey <py.emkey@gmail.com>
2014-03-09 11:45:29 +0100

    Use get and post method for next_round

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,5 @@ Rails.application.routes.draw do
   get 'results', to: 'games#results'
   
   resources :games
-  get 'next_round', to: "game_cards#next_round", as: :next_round
+  match 'next_round', to: "game_cards#next_round", as: :next_round, via: [:get, :post]
 end
